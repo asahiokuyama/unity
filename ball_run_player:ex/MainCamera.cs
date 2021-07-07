c18767d99f32d5661165ff81bc4261a199a0b431
Asahi Okuyama <59007694+asahiokuyama@users.noreply.github.com>
2021-07-07 17:05:43 +0900

    Add files via upload

---
+++ ball_run_player:ex/MainCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject player;
+    Vector3 pos;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        pos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //「pos」 のz座標だけを現在のプレイヤーのz座標に-2してセットする
+        pos.z = player.transform.position.z - 8;
+
+        //ここがないところ
+        pos.y = player.transform.position.y + 5;
+        pos.x = player.transform.position.x;
+
+
+        //現在のカメラの座標に「pos」 をセットする
+        gameObject.transform.position = pos;
+    }
+}
---
+++ ball_run_player:ex/player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //変数「pos」をつくる
+        Vector3 pos;
+        //変数「pos」に変数「rb」(PlayerのRigidbody)の座標をセットする
+        pos = rb.position;
+        //変数「pos」のx座標にマウスのx座標を3D空間の座標に変換してセットする
+        pos.x = Camera.main.ScreenToWorldPoint(Input.mousePosition + Camera.main.transform.forward * 0.5f).x;
+        //変数「rb」(PlayerのRigidbody)のMovePositionに変数「pos」の値をセットする
+        rb.MovePosition(pos);
+
+        //Vector3型の変数「vel」をつくる
+        Vector3 vel;
+        //「vel」に変数rb(PlayerのRigidbodyのvelocity（速さ）をセットする)
+        vel = rb.velocity;
+        //「vel」のz座標に3をセットする
+        vel.z = 3;
+        //「rb」(PlayerのRigidbodyのvelocity(速さ)に「vel」 の情報をセットする
+        rb.velocity = vel;
+    }
+}
