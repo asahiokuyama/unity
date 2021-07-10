df4136ff769fe78208d22787c3646b1908afb6eb
Asahi Okuyama <59007694+asahiokuyama@users.noreply.github.com>
2021-07-10 09:42:57 +0900

    Add files via upload

---
+++ gamesetさせる/cube_hp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cube_hp : MonoBehaviour
+{
+    public int cubehp = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(cubehp);
+        if(cubehp < 1){
+            SceneManager.LoadScene("gameover");
+        }
+    }
+
+    public void hpdown(){
+        cubehp--;
+    }
+}
---
+++ gamesetさせる/factory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class factory : MonoBehaviour
+{
+    public GameObject ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // InvokeRepeating("Spawn",0f,1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Spawn();
+        }
+    }
+
+    public void Spawn(){
+        Instantiate(ball,transform.position,transform.rotation);
+    }
+}
