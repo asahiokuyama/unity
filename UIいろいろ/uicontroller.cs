41b12ee29c56af78a657e7541221cb43cc635c8b
Asahi Okuyama <59007694+asahiokuyama@users.noreply.github.com>
2021-07-14 15:38:27 +0900

    Update uicontroller.cs

--- UIいろいろ/uicontroller.cs
+++ UIいろいろ/uicontroller.cs
@@ -7,19 +7,23 @@ using UnityEngine.SceneManagement;
 public class uicontroller : MonoBehaviour
 {
     GameObject enemy_hp;
+    GameObject reset_button;
     int hp = 10;
     // Start is called before the first frame update
     void Start()
     {
         enemy_hp = GameObject.Find("enemy_hp");
+        reset_button = GameObject.Find("reset");
     }
 
     // Update is called once per frame
     void Update()
     {
         if(hp > 0){
+            reset_button.SetActive(false);
             enemy_hp.GetComponent<Text>().text = "HP:" + hp;
         }else{
+            reset_button.SetActive(true);
             enemy_hp.GetComponent<Text>().text = "Fin!!";
         }
     }
