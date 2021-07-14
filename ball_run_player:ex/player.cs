1bbcdb1161534b2f3cbaa30713f68fab5bb8632f
Asahi Okuyama <59007694+asahiokuyama@users.noreply.github.com>
2021-07-14 16:22:02 +0900

    Update player.cs

--- ball_run_player:ex/player.cs
+++ ball_run_player:ex/player.cs
@@ -14,22 +14,16 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //変数「pos」をつくる
         Vector3 pos;
-        //変数「pos」に変数「rb」(PlayerのRigidbody)の座標をセットする
         pos = rb.position;
         //変数「pos」のx座標にマウスのx座標を3D空間の座標に変換してセットする
         pos.x = Camera.main.ScreenToWorldPoint(Input.mousePosition + Camera.main.transform.forward * 0.5f).x;
         //変数「rb」(PlayerのRigidbody)のMovePositionに変数「pos」の値をセットする
         rb.MovePosition(pos);
 
-        //Vector3型の変数「vel」をつくる
         Vector3 vel;
-        //「vel」に変数rb(PlayerのRigidbodyのvelocity（速さ）をセットする)
         vel = rb.velocity;
-        //「vel」のz座標に3をセットする
         vel.z = 3;
-        //「rb」(PlayerのRigidbodyのvelocity(速さ)に「vel」 の情報をセットする
         rb.velocity = vel;
     }
 }
