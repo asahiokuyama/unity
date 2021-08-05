052dd16cc62429c8574bdb325e61ecda18d387a6
Asahi Okuyama <59007694+asahiokuyama@users.noreply.github.com>
2021-08-05 17:43:08 +0900

    Update text_2pattern.cs

--- text_2pattern.cs
+++ text_2pattern.cs
@@ -1,32 +1,36 @@
-﻿using System.Collections;
+//①Scene遷移を選択した場合
+//②UI表示を選択した場合
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;//①
 
 public class move : MonoBehaviour
 {
-    GameObject fintext;
+    GameObject fintext;//②
     // Start is called before the first frame update
     void Start()
     {
-        fintext = GameObject.Find("Fin");
+        fintext = GameObject.Find("Fin");//②
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //これ関係ない
         transform.position += new Vector3(Input.GetAxis("Vertical")*Time.deltaTime, 0, 0);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        //②
         if (collision.gameObject.CompareTag("fin1"))
         {
             fintext.GetComponent<Text>().text = "GameClear";
         }
-
+        
+        //①
         if (collision.gameObject.CompareTag("fin2"))
         {
             SceneManager.LoadScene("gameclear");
