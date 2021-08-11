adda6f5a32fb7cc2b7caa251294a60b20edd3ca0
Asahi Okuyama <59007694+asahiokuyama@users.noreply.github.com>
2021-08-11 19:51:10 +0900

    Add files via upload

---
+++ 3回通過したら終わる/move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    float timer = 0;
+    int count = 0;
+    bool flag = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(flag == true){
+            timer += Time.deltaTime; 
+            Debug.Log("Time:"+(int)timer);
+        }
+        
+        transform.position += new Vector3(Input.GetAxis("Vertical")*Time.deltaTime,0,0);
+       
+       
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.gameObject.CompareTag("goal")){
+            count++;
+            Debug.Log("s");
+            if(count >= 3){
+            Debug.Log("goal!!!");
+            flag = false;
+        }
+        }
+        
+    }
+}
