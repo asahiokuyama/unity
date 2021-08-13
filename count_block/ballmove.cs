4248bf58ba0bc2ef158d5e0f7b81c5ca1352cf10
Asahi Okuyama <59007694+asahiokuyama@users.noreply.github.com>
2021-08-13 16:18:25 +0900

    Add files via upload

---
+++ count_block/ballmove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ballmove : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    GameObject count_text;
+
+    int count_number = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(0, 0, -5, ForceMode.Impulse);
+
+        count_text = GameObject.Find("count");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(count_number);
+        if (count_number < 4)
+        {
+            count_text.GetComponent<Text>().text = "Breaked block:" + count_number;
+        }
+        else
+        {
+            count_text.GetComponent<Text>().text = "FIN!!";
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("block"))
+        {
+            count_number++;
+        }
+    }
+
+}
