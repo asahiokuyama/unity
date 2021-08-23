5d526d4a55e135370cdf86806683920cf90a1d9d
Asahi Okuyama <59007694+asahiokuyama@users.noreply.github.com>
2021-08-23 18:41:46 +0900

    Update ballmove.cs
    ここって書いてあるところを書き足す

--- count_block/ballmove.cs
+++ count_block/ballmove.cs
@@ -1,28 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI;//ここ
 
 public class ballmove : MonoBehaviour
 {
     public Rigidbody rb;
-
+    
+    
+    
+    ///////////////////ここ
     GameObject count_text;
 
     int count_number = 0;
+    //////////////////
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rb.AddForce(0, 0, -5, ForceMode.Impulse);
+        
+        
 
+           ////////////////ここ
         count_text = GameObject.Find("count");
+        //////////////
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log(count_number);
+        
+        
+        
+        /////////////////////////ここ
         if (count_number < 4)
         {
             count_text.GetComponent<Text>().text = "Breaked block:" + count_number;
@@ -31,8 +46,16 @@ public class ballmove : MonoBehaviour
         {
             count_text.GetComponent<Text>().text = "FIN!!";
         }
+        ////////////////////////////
+        
+        
+        
     }
+    
+    
+    
 
+     ////////////////////////////////ここ
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("block"))
@@ -40,5 +63,9 @@ public class ballmove : MonoBehaviour
             count_number++;
         }
     }
+    ///////////////////////////////////
+    
+    
+    
 
 }
