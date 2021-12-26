c0da309a520bbd0c8442269b9014885a6e5b8975
Asahi Okuyama <59007694+asahiokuyama@users.noreply.github.com>
2021-12-26 12:17:19 +0900

    Update look_player.cs

--- 追尾する砲台/look_player.cs
+++ 追尾する砲台/look_player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class look_player : MonoBehaviour
 {
     public GameObject targert;//targetを入れる変数
-    bool rock = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,20 +20,11 @@ public class look_player : MonoBehaviour
         //ずっとこのオブジェクトはtargetを追って回転する
         transform.LookAt(targert.transform);
         
-        //ここがポイントで、変数をつかって疑似ロック的なことをしている。これをしないと正常に動かない
-        if(rock == false){
-            if(transform.rotation.x <=-0.3){
-                rock = true;
-              
-            }
-        }
-        if(rock == true){
+        if(transform.rotation.x <=-0.3){
             this.transform.localEulerAngles = new Vector3(-30,0,0);
-            rock = false;
+
         }
+
      
-            
-        //
-        
     }
 }
